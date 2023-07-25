ff88bd28be71afc68d8739b8b679db3329c6891f
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-07-25 18:21:41 +0200

    added bonus JSnack3

--- js/JSnack3.js
+++ js/JSnack3.js
@@ -1,7 +1,22 @@
+// generate array of number
+
+const numbersAmount = prompt("Quanti numeri vuoi generare ?");
+
+const maxNumber = prompt("Dimmi il numero più alto a cui possono arrivare");
+
+let numbers = [];
+
+for (let i = 0; i < numbersAmount; i++) {
+  let newNumber = Math.floor(Math.random() * maxNumber + 1);
+  numbers.push(newNumber);
+}
+
+console.log(numbers);
+
 // array of numbers
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-console.log(numbers);
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// console.log(numbers);
 
 let sumNumbers = 0;
 
